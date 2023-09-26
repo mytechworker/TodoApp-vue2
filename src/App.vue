9fafe14b2c127e2a0bd12f9306ab252217c2be80
mytechworker <mytechworker@gmail.com>
2023-09-26 17:11:22 +0530

    Todo App In Vue2

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <todo-app />
+  </div>
+</template>
+
+<script>
+import TodoApp from "./components/TodoApp.vue";
+export default {
+  name: 'App',
+  components: {
+    TodoApp,
+  },
+}
+</script>
+
+<style>
+
+</style>
